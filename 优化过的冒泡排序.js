87e6bbb70bfe5b22b5aa153fd5efc08cc14d784c
尹迟 <yinchi@yinchideMacBook-Pro.local>
2017-02-19 14:03:50 +0800

    家里

---
+++ 优化过的冒泡排序.js
@@ -0,0 +1,31 @@
+/**
+ * Created by yinchi on 2017/2/19.
+ */
+var arr = [];
+function creatArray(len,start,end) {
+    for(let i = 0;i < len; i++){
+        arr.push(Math.floor(Math.random()*(start+end)));
+    }
+}
+function fntime(fn) {
+    let start = new Date().getTime();
+    fn();
+    let end = new Date().getTime();
+    console.log(end-start+"毫秒!")
+}
+function maopao() {
+    for(let i = 0;i < arr.length;i++){
+        for (let j = 1;j < arr.length-i; j++){
+            if(arr[j] < arr[j-1]){
+                [arr[j],arr[j-1]] = [arr[j-1],arr[j]]
+            }
+        }
+    }
+    console.log("------------------------------------------------------------------------------------")
+    console.log("我是冒泡算法"+arr)
+}
+function main() {
+    creatArray(10000,0,10000);
+    fntime(maopao)
+}
+main()
